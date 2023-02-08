16cb1e1378f0b9a5cd311142b5351d9a8708b423
micomico802 <sho.mizumoto802@gmail.com>
2023-02-08 21:05:09 +0900

    追加

--- src/main/domain/ClientService.java
+++ src/main/domain/ClientService.java
@@ -3,5 +3,6 @@ package main.domain;
 public class ClientService {
     public void clientStart() {
         System.out.println("クライアントスタートです");
+        System.out.println("クライアントスタートです");
     }
 }
